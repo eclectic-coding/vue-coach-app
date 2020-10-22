fcb44038f27829d2e9d8d5c46ce4a6b10ee25157
Chuck Smith <chuck@eclecticsaddlebag.com>
2020-10-22 18:37:11 -0400

    Fix nested route bug

--- src/router.js
+++ src/router.js
@@ -1,9 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import CoachesDetail from '@/pages/coaches/CoachesDetail';
 import CoachesList from '@/pages/coaches/CoachesList';
+import CoachesDetail from '@/pages/coaches/CoachesDetail';
+import ContactCoaches from '@/pages/requests/ContactCoaches';
 import CoachesRegistration from '@/pages/coaches/CoachesRegistration';
-// import ContactCoaches from '@/pages/requests/ContactCoaches';
 import RequestsReceived from '@/pages/requests/RequestsReceived';
 import NotFound from '@/pages/NotFound';
 
@@ -12,10 +12,17 @@ const router = createRouter({
   routes: [
     { path: '/', redirect: '/coaches' },
     { path: '/coaches', component: CoachesList },
-    { path: '/coaches/:id', component: CoachesDetail },
+    {
+      path: '/coaches/:id', component: CoachesDetail,
+      children: [
+        {
+          path: 'contact', component: ContactCoaches
+        }
+      ]
+    },
     { path: '/register', component: CoachesRegistration },
     { path: '/requests', component: RequestsReceived },
-    { path: '/:notFound(.*)', component: NotFound },
+    { path: '/:notFound(.*)', component: NotFound }
   ]
 });
 
